1e0a612e367684e76b30933644c4e3ef2f8eaa40
sushilsridhar <sushil.sridhar.sh@gmail.com>
2023-05-11 21:23:01 +0530

    initial setup

---
+++ src/main/java/com/springbasics/HelloController.java
@@ -0,0 +1,23 @@
+package com.springbasics;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/you")
+    public String helloYou(@RequestParam("name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/U/{name}")
+    public String helloU(@PathVariable("name") String name) {
+        return "Hello: PathParam " + name + "!";
+    }
+}
